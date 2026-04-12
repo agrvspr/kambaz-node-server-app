5ec5c4a7a7c1dc7ff09cde25dc54de787c515d17
Daniel Jea <daniel.jea786@gmail.com>
2026-04-11 23:34:12 -0400

    a6 fixed

--- kambaz/courses/dao.js
+++ kambaz/courses/dao.js
@@ -1,30 +1,14 @@
 import model from "./model.js";
-import { v4 as uuidv4 } from "uuid";
-export default function CoursesDao(db) {
-    function findAllCourses() {
-        return model.find({}, { name: 1, description: 1 });
-    }
-    async function findCoursesForEnrolledUser(userId) {
-        const { enrollments } = db;
-        const courses = await model.find({}, { name: 1, description: 1 });
-        const enrolledCourses = courses.filter((course) =>
-            enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
-        return enrolledCourses;
-    }
-    function createCourse(course) {
-        const newCourse = { ...course, _id: uuidv4() };
-        return model.create(newCourse);
-    }
-    function deleteCourse(courseId) {
-        const { enrollments } = db;
-        db.enrollments = enrollments.filter((enrollment) => enrollment.course !== courseId);
-        return model.deleteOne({ _id: courseId });
-    }
-    function updateCourse(courseId, courseUpdates) {
-        const { courses } = db;
-        const course = courses.find((course) => course._id === courseId);
-        Object.assign(course, courseUpdates);
-        return course;
-    }
-    return { findAllCourses, findCoursesForEnrolledUser, createCourse, deleteCourse, updateCourse };
+
+export function findAllCourses() {
+  return model.find();
+}
+export function createCourse(course) {
+  return model.create(course);
+}
+export function deleteCourse(courseId) {
+  return model.deleteOne({ _id: courseId });
+}
+export function updateCourse(courseId, courseUpdates) {
+  return model.updateOne({ _id: courseId }, { $set: courseUpdates });
 }
